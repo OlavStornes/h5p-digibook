fccf15c2801663a1cc36b3f1cd5db750b23d4e68
Olav Stornes <olav.d.stornes@gmail.com>
2018-09-10 12:52:00 +0200

    Initial support of multiple columns

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,11 +9,11 @@ export default class DigiBook extends H5P.EventDispatcher {
    * @param {string} contentId
    * @param {object} contentData
    */
-  
+
   constructor(config, contentId, contentData = {}) {
     super();
     // Find all types of content inside a column
-    this.columnFinder = function(arrElems) {
+    this.columnFinder = function (arrElems) {
       let elemArray = [];
       arrElems.forEach(e => {
         elemArray.push(e.content);
@@ -22,35 +22,40 @@ export default class DigiBook extends H5P.EventDispatcher {
     };
 
     //Retrofit the content id to the column content
-    this.injectId = function() {
-      let tmp = this.colelem.getElementsByClassName('h5p-column-content');
+    this.injectId = function () {
+      let tmp = this.colelem[0].getElementsByClassName('h5p-column-content');
       for (let i = 0; i < tmp.length; i++) {
-        tmp[i].id = config.Column.params.content[i].content.subContentId;
+        tmp[i].id = config.chapters[0].params.content[i].content.subContentId;
 
         //Needed to make elements redirectable
         tmp[i].setAttribute('tabindex', '-1');
       }
     };
-    
-    this.colelem = document.createElement('div');
-    
-    this.bookpage = H5P.newRunnable(config.Column, contentId, H5P.jQuery(this.colelem), contentData);
-    this.sidebar = new SideBar(this.columnFinder(config.Column.params.content), contentId);
+
+    // debugger;
+    this.colelem = [];
+    for (let i = 0; i < config.chapters.length; i++) {
+      this.colelem.push(document.createElement('div'));
+      this.bookpage = H5P.newRunnable(config.chapters[i], contentId, H5P.jQuery(this.colelem[i]), contentData);
+    }
+    this.sidebar = new SideBar(this.columnFinder(config.chapters[0].params.content), contentId);
     this.topbar = new TopBar();
 
     this.injectId();
-    
+
     /**
      * Attach library to wrapper
      *
      * @param {jQuery} $wrapper
      */
-    this.attach = function($wrapper) {
+    this.attach = function ($wrapper) {
       $wrapper.get(0).classList.add('h5p-book-page');
       $wrapper.get(0).appendChild(this.topbar.div);
       $wrapper.get(0).appendChild(this.sidebar.div);
-      $wrapper.get(0).appendChild(this.colelem);
 
+      this.colelem.forEach(element => {
+        $wrapper.get(0).appendChild(element);
+      });
     };
   }
 
