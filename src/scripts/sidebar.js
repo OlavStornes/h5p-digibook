89f70be9b736f65881022df22f0a4a5aa4925fbd
Olav Stornes <olav.d.stornes@gmail.com>
2018-10-15 16:11:12 +0200

    Add progress handler on chapter title
    close #57, close #58

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -97,22 +97,43 @@ class SideBar extends H5P.EventDispatcher {
     const targetElem = this.chapterElems[newChapter];
     this.editChapterStatus(targetElem, false);
   }
+
+  updateChapterTitle(targetSection, targetChapter) {
+    const x = this.chapters[targetChapter];
+    if (x.hasTasks) {
+      let targetElem = this.chapterElems[targetChapter].getElementsByClassName('h5p-digibook-navigation-chapter-title')[0];
+      targetElem = targetElem.getElementsByClassName('h5p-digibook-navigation-chapter-progress')[0];
+      if (x.tasksLeft >= 1) {
+        x.tasksLeft =- 1;
+        targetElem.classList.remove('icon-chapter-blank');
+        targetElem.classList.add('icon-chapter-started');
+      }
+      else {
+        targetElem.classList.remove('icon-chapter-blank');
+        targetElem.classList.add('icon-chapter-done');
+      }
+    }
+  }
+
   /**
    * Set a section progress indicator
    * 
    * @param {string} targetId 
-   * @param {string} chapter 
+   * @param {string} targetChapter 
    */
-  setSectionStatusByID(targetId, chapter) {
-    for (let i = 0; i < this.chapters[chapter].sections.length; i++) {
-      const element = this.chapters[chapter].sections[i];
+  setSectionStatusByID(targetId, targetChapter) {
+    for (let i = 0; i < this.chapters[targetChapter].sections.length; i++) {
+      const element = this.chapters[targetChapter].sections[i];
       if (element.subContentId === targetId) {
-        const tmp = this.chapterElems[chapter].getElementsByClassName('h5p-digibook-navigation-section')[i];
+        element.taskDone = true;
+        const tmp = this.chapterElems[targetChapter].getElementsByClassName('h5p-digibook-navigation-section')[i];
         const icon = tmp.getElementsByTagName('span')[0];
         if (icon) {
           icon.classList.remove('icon-chapter-blank');
           icon.classList.add('icon-chapter-done');
         }
+
+        this.updateChapterTitle(i, targetChapter);
       }
     }
   }
