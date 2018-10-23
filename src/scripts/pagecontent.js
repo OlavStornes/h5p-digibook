fc6ca4424c2d3157eb9c273cbfa06ab8deab55c0
Olav Stornes <olav.d.stornes@gmail.com>
2018-10-23 09:24:28 +0200

    Add constructor and ish5p-task

--- src/scripts/pagecontent.js
+++ src/scripts/pagecontent.js
@@ -1,4 +1,31 @@
 class PageContent extends H5P.EventDispatcher {
+  /**
+   * @constructor
+   *
+   * @param {object} config
+   * @param {string} contentId
+   * @param {object} contentData
+   */
+  constructor(config, contentId, contentData = {}) {
+    super();
+    this.parent = parent;
+    this.behaviour = config.behaviour;
+
+    // H5P-instances (columns)
+    this.instances = [];
+    // Div-elements of the abovementioned h5p-instances
+    this.columnElements = [];
+
+  }
+
+
+  isH5PTask(H5PObject) {
+    if (typeof H5PObject.getMaxScore === 'function') {
+      return H5PObject.getMaxScore() > 0;
+    }
+    return false;
+  }
+
 
 }
 
